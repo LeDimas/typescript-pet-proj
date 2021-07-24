ad8a6db48d041655ec0adceb4039afcb5c5f58a8
LeDimas <morph232@inbox.lv>
2021-07-25 01:36:38 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Card, { CardVariant } from "./components/Card";
+import React , {FC, useState , useEffect} from 'react';
+import {BrowserRouter , NavLink, Route} from 'react-router-dom'
+import { ITodo, IUser } from "./types/types";
+import axios from "axios";
+import List from "./components/List";
+import UserItem from "./components/UserItem";
+import TodoItem from "./components/TodoItem";
+import EventExample from "./components/EventsExample";
+import UserPage from "./components/UserPage";
+import TodosPage from "./components/TodosPage";
+import SingleUserPage from "./components/SingleUserPage";
+import SingleTodoPage from "./components/SingleTodoPage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+export interface AppProps {
+  
 }
 
+
+
+const App: FC<AppProps> = () => (
+
+  <BrowserRouter>
+    <div>
+      <div>
+        <NavLink to='/users'>Users</NavLink>
+        <NavLink to='/todos'>Todos</NavLink>
+      </div>
+      <Route path={'/users'} exact>
+        <UserPage />
+      </Route>
+
+      <Route path={'/todos'} exact>
+        <TodosPage />
+      </Route>
+
+      <Route path={'/user/:id'} exact>
+        <SingleUserPage />
+      </Route>
+
+      <Route path={'/todo/:id'} exact>
+        <SingleTodoPage />
+      </Route>
+
+    </div>
+  </BrowserRouter>
+)
+ 
 export default App;
